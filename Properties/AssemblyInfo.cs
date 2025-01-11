abb5e190977c82bd351d6a2dbb216b6b9ece685d
sonesoul <smippy098@gmail.com>
2025-01-11 15:37:48 +0200

    update assembly file version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -30,4 +30,4 @@ using System.Runtime.InteropServices;
 //      Редакция
 //
 [assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
