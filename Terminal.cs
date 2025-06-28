d662cac565805c54694aaa6929d42c859e61f829
sonesoul <smippy098@gmail.com>
2025-06-28 20:36:50 +0300

    replace HideFromTaskbar and HideButtons with WindowStyle enum

--- Terminal.cs
+++ Terminal.cs
@@ -22,33 +22,10 @@ namespace KernelTerminal
         /// </summary>
         public static ConsoleColor BackgroundColor { get => Console.BackgroundColor; set => Console.BackgroundColor = value; }
 
-        /// <summary>
-        /// Gets a value indicating whether the console is currently opened.
-        /// </summary>
-        /// <returns>true if the console is opened; otherwise, false.</returns>
         public static bool IsOpened { get; private set; } = false;
-        
-        /// <summary>
-        /// Gets or sets a value indicating whether to hide the console's system buttons.
-        /// </summary>
-        public static bool HideButtons { get; set; } = true;
-        /// <summary>
-        /// Gets or sets a value indicating whether to hide the console from the taskbar.
-        /// </summary>
-        public static bool HideFromTaskbar { get; set; } = true;
-
-        /// <summary>
-        /// Gets or sets the title of the console window.
-        /// </summary>
         public static string Title { get; set; } = "Terminal";
 
-        /// <summary>
-        /// Occurs when the console is opened.
-        /// </summary>
         public static Action Opened { get; set; }
-        /// <summary>
-        /// Occurs when the console is closed.
-        /// </summary>
         public static Action Closed { get; set; }
 
         /// <summary>
@@ -57,26 +34,37 @@ namespace KernelTerminal
         ///<returns>Console window handle of the console if it is opened; otherwise, null.</returns>
         public static IntPtr? WindowHandle { get; private set; } = null;
 
+        private static WindowStyle _currentStyle;
         private readonly static object _writeLock = new object();
         
         #endregion
 
-        #region Open/Close
-        /// <summary>
-        /// Opens the console if it is not already open.
-        /// </summary>
-        /// <returns>true if the console was successfully opened; otherwise, false.</returns>
-        public static bool Open()
+        public static bool Open(WindowStyle style)
         {
             if (IsOpened)
                 return false;
 
-            bool opened = NativeInterop.AllocConsole();
-
-            if (!opened)
+            if (!NativeInterop.AllocConsole())
                 return false;
 
-            CreateWindow();
+            IntPtr windowHandle;
+            WindowHandle = windowHandle = NativeInterop.GetNewWindow();
+
+            if (style.HasFlag(WindowStyle.ButtonsHidden))
+            {
+                NativeInterop.HideButtons(windowHandle);
+            }
+            if (style.HasFlag(WindowStyle.ProcessHidden))
+            {
+                NativeInterop.HideFromTaskbar(windowHandle);
+            }
+
+            _currentStyle = style;
+            
+            ForegroundColor = ConsoleColor.Yellow;
+
+            Console.Title = Title;
+            Console.OutputEncoding = Encoding.UTF8;
 
             IsOpened = true;
 
@@ -84,77 +72,39 @@ namespace KernelTerminal
 
             return true;
         }
-        /// <summary>
-        /// Closes the console if open.
-        /// </summary>
-        /// <returns>true if the console was successfully closed; otherwise, false.</returns>
         public static bool Close()
         {
             if (!IsOpened)
                 return false;
 
-            bool closed = NativeInterop.FreeConsole();
-
-            if (!closed)
+            if (!NativeInterop.FreeConsole())
                 return false;
             
             NativeInterop.ResetConsole();
 
+            _currentStyle = WindowStyle.None;
             WindowHandle = null;
-
             IsOpened = false;
+
             Closed?.Invoke();
 
             return true;
         }
-        /// <summary>
-        /// Toggles the console state (opens or closes it).
-        /// </summary>
-        public static void Toggle()
-        {
-            if (IsOpened)
-                Close();
-            else
-                Open();
-        }
-        /// <summary>
-        /// Closes the console if it is open and opens a new one.
-        /// </summary>
+
         public static void New()
         {
-            if (IsOpened)
-                Close();
+            WindowStyle style = _currentStyle;
 
-            Open();
+
         }
-        #endregion
 
-        private static void CreateWindow()
-        {
-            IntPtr windowHandle;
-            WindowHandle = windowHandle = NativeInterop.GetNewWindow();
-
-            if (HideButtons)
-            {
-                NativeInterop.HideButtons(windowHandle);
-            }
-            if (HideFromTaskbar)
-            {
-                NativeInterop.HideFromTaskbar(windowHandle);
-            }
-
-            ForegroundColor = ConsoleColor.Yellow;
-
-            Console.Title = Title;
-            Console.OutputEncoding = Encoding.UTF8;
-        }
+        public static WindowStyle GetCurrentStyle() => _currentStyle;
 
         #region Output
 
         /// <summary>
         /// Writes the specified value to the console without a newline, using the specified foreground color.
         /// </summary>
-        /// <returns>Task representing the asynchronous operation.</returns>
         public static void Write(string value, ConsoleColor foreColor = ConsoleColor.Gray, ConsoleColor backColor = ConsoleColor.Black)
         {
             WriteColored(() => Console.Write(value), foreColor, backColor);
@@ -162,7 +112,6 @@ namespace KernelTerminal
         /// <summary>
         /// Writes the specified value to the console followed by a newline, using the specified foreground color.
         /// </summary>
-        /// <returns>Task representing the asynchronous operation.</returns>
         public static void WriteLine(string value, ConsoleColor foreColor = ConsoleColor.Gray, ConsoleColor backColor = ConsoleColor.Black)
         {
             WriteColored(() => Console.WriteLine(value), foreColor, backColor);
@@ -170,7 +119,6 @@ namespace KernelTerminal
         /// <summary>
         /// Writes a newline symbol to the console.
         /// </summary>
-        /// <returns>Task representing the asynchronous operation.</returns>
         public static void WriteLine() => Write(Environment.NewLine);
 
         private static void WriteColored(Action writeAction, ConsoleColor foreColor, ConsoleColor backColor)
