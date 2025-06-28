61c0ef261cc7d013800ef15a2d07daf80da7b157
sonesoul <smippy098@gmail.com>
2025-06-28 21:39:51 +0300

    fix WindowStyle

--- WindowStyle.cs
+++ WindowStyle.cs
@@ -5,7 +5,7 @@ namespace KernelTerminal
     public enum WindowStyle : byte
     {
         None = 0,
-        ButtonsHidden = 1,
-        ProcessHidden= 2,
+        ButtonsHidden = 1 << 0,
+        ProcessHidden= 1 << 1,
     }
 }
