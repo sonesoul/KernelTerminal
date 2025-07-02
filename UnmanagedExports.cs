fb7c1c9051d7ae34113ebd902ee6d57267419023
sonesoul <smippy098@gmail.com>
2025-07-02 11:55:15 +0300

    add exported methods

---
+++ UnmanagedExports.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace KernelTerminal
+{
+    public static class UnmanagedExports
+    {
+        [UnmanagedCallersOnly(EntryPoint = nameof(OpenTerminal))]
+        public static bool OpenTerminal(int style) => Terminal.Open((WindowStyle)style);
+
+        [UnmanagedCallersOnly(EntryPoint = nameof(CloseTerminal))]
+        public static bool CloseTerminal() => Terminal.Close();
+
+
+        [UnmanagedCallersOnly(EntryPoint = nameof(SetTerminalFontSize))]
+        public static void SetTerminalFontSize(short width, short height) => Terminal.SetFontSize(width, height);
+        [UnmanagedCallersOnly(EntryPoint = nameof(SetTerminalVisible))]
+        public static void SetTerminalVisible(int visible) => Terminal.SetVisible(visible != 0);
+
+
+        [UnmanagedCallersOnly(EntryPoint = nameof(GetWindowHandle))]
+        public static IntPtr GetWindowHandle() => Terminal.WindowHandle ?? new IntPtr(-1);
+    }
+}
