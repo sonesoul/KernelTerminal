671c6c234f1ec8681415089cfb3e782b7bf52fb8
sonesoul <smippy098@gmail.com>
2025-07-02 11:54:18 +0300

    change base enum type from byte to int

--- WindowStyle.cs
+++ WindowStyle.cs
@@ -1,9 +1,9 @@
 ﻿namespace KernelTerminal
 {
-    public enum WindowStyle : byte
+    public enum WindowStyle : int
     {
         Default = 0,
         ButtonsHidden = 1 << 0,
-        ProcessHidden= 1 << 1,
+        ProcessHidden = 1 << 1,
     }
 }
