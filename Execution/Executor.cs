a7243a71c4bf7589d00b62c0d1068cc41ba8e286
sonesoul <smippy098@gmail.com>
2026-07-06 20:59:13 -0600

    add InvocationRequested event for user logic

--- Execution/Executor.cs
+++ Execution/Executor.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace KernelTerminal.Execution
 {
     public static class Executor
     {
         public static InvocationParser Parser { get; set; } = new();
+        public static Action<string> InvocationRequested { get; set; } = Execute;
+        public static Dictionary<string, ICommand> Commands { get; } = new();
 
-        public static Dictionary<string, ICommand> Commands { get; } = new()
+        public static void Invoke(string invocation)
         {
-        };
+            InvocationRequested.Invoke(invocation);
+        }
 
         public static void Execute(string invocation)
         {
